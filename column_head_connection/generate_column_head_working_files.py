1da28e45f9dcc4d2b258a3b7e21b4d2c5f377a91
Josh.Finnin <joshua.finnin@gmail.com>
2025-09-03 09:10:11 +1000

    Updated to consider ALS and new structure correctly

--- column_head_connection/generate_column_head_working_files.py
+++ column_head_connection/generate_column_head_working_files.py
@@ -2,15 +2,28 @@
 
 import duckdb
 import logging
+import time
 
 from inputs import (FULL_BEAM_FORCES_PARQUET, BEAM_ENDS_PARQUET, NODAL_FORCE_PARQUET, BEAM_PROPERTY_PARQUET)
 from inputs import (BF_PERM_PARQ_FILE_DICT, BP_PERM_PARQ_FILE_DICT,
                     BF_EXT_ALS_PARQ_FILE_DICT, BP_EXT_ALS_PARQ_FILE_DICT, NODE_DICT)
-from inputs import EXCLUDED_BEAM_DICT, TARGET_GROUPS, RESULT_CASE_FILTER, COL_HEAD_LOCATION
+from inputs import EXCLUDED_BEAM_DICT, TARGET_GROUPS, RESULT_CASE_FILTER, COL_HEAD_LOCATION, ALS_ONLY
 
 
 def configure_logging():
     logging.basicConfig(level=logging.INFO)
+
+
+def process_time(func):
+
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        duration = end - start
+        log.info(f"Duration: {duration:.2f}s")
+
+    return wrapper
 
 
 def get_perm_beam_end_query(location: str, bp_parq_file: str, node_dict: dict, target_groups: str,
@@ -18,9 +31,7 @@ def get_perm_beam_end_query(location: str, bp_parq_file: str, node_dict: dict, t
     """Gets the SQL write_full_beam_forces_query for finding the relevant ends of the beam.
     Have updated the query to only pull the properties from a single model."""
 
-    models = BP_PERM_PARQ_FILE_DICT.keys()
-
-    nodes = {m_name: node_dict[location] for m_name in models if "ALS" not in m_name}
+    nodes = node_dict[location]
 
     # This query needs to be updated
     beam_end_query = f"""
@@ -56,8 +67,8 @@ def get_full_beam_properties_query(bp_parq_file: str, target_groups: str) -> str
     return property_query
 
 
-def get_full_beam_force_query(bf_perm_parq_files: dict, bf_als_parq_files: dict,
-                              beam_numbers: tuple, target_groups: str, result_case_filter: str) -> str:
+def get_full_beam_force_query(bf_perm_parq_files: dict, bf_als_parq_files: dict, bp_als_parq_files: dict,
+                              beam_numbers: tuple, target_groups: str, result_case_filter: str, als_only=False) -> str:
     """Gets the full set of beam forces for beams from the series of bf_parqs, using filtering criteria."""
 
     # First query the permanent models for the end forces that occur for our target beams
@@ -73,18 +84,24 @@ def get_full_beam_force_query(bf_perm_parq_files: dict, bf_als_parq_files: dict,
     als_prequery = "\n UNION ALL ".join(f"""SELECT
                                             BeamIDNumber AS BeamNumber, ResultCaseName, ResultCase, Position, 
                                             '{model_name}' AS Model, Fx, Fy, Fz, Mx, My, Mz 
-                                            FROM '{bf_als_parq_file}'
+                                            FROM '{bf_als_parq_file}' AS BF
+                                            JOIN '{bp_als_parq_files[model_name]}' 
+                                            AS BP ON BP.BeamNumber = BF.BeamNumber
                                             WHERE Position IN (0.0, 1.0) AND ResultCaseName NOT LIKE '%BIF%'
                                             AND BeamIDNumber IN {beam_numbers}""" for model_name,
                                             bf_als_parq_file in bf_als_parq_files.items())
 
-    prequery = perm_prequery + "\n UNION ALL \n" + als_prequery
+    # Added in this logic to ensure that we can query only the ALS models if we want to, but for the elements
+    # that occur in the permanent model at the location we identify
+    if als_only:
+        prequery = als_prequery
+    else:
+        prequery = perm_prequery + "\n UNION ALL \n" + als_prequery
 
     full_beam_force_query = f"""SELECT BF.BeamNumber, ResultCaseName, ResultCase, Position, 
     BF.Model, Fx, Fy, Fz, Mx, My, Mz 
     FROM ({prequery}) AS BF 
     JOIN FULL_BEAM_PROPERTIES AS BP ON BF.BeamNumber = BP.BeamNumber 
-    AND BP.Model = BF.Model 
     AND ResultCaseName NOT LIKE '%BIF%'
     AND ResultCaseName NOT IN {result_case_filter}
     AND BP.GroupName IN {target_groups}"""
@@ -108,8 +125,8 @@ def get_nodal_beam_force_query() -> str:
         Fx * Sign AS Fx, Fy * Sign AS Fy, Fz * Sign AS Fz, 
         Mx * Sign AS Mx, My * Sign AS My, Mz * Sign AS Mz
         FROM FULL_BEAM_FORCES AS BF
-        JOIN FULL_BEAM_PROPERTIES AS BP ON BP.BeamNumber = BF.BeamNumber AND BP.Model = BF.Model
-        JOIN BEAM_ENDS AS BE ON BE.BeamNumber = BF.BeamNumber AND BE.Model = BF.Model
+        JOIN FULL_BEAM_PROPERTIES AS BP ON BP.BeamNumber = BF.BeamNumber
+        JOIN BEAM_ENDS AS BE ON BE.BeamNumber = BF.BeamNumber
         WHERE BF.Position = BE.BeamEnd"""
 
     return nodal_beam_force_query
@@ -148,12 +165,13 @@ if __name__ == '__main__':
 
         # Querying the beam numbers first facilitates predicate pushdown as I can inject the values as literals
         # So the subsequent queries should execute significantly faster
-        full_beam_force_query = get_full_beam_force_query(BF_PERM_PARQ_FILE_DICT, BP_EXT_ALS_PARQ_FILE_DICT,
-                                                          beam_numbers, TARGET_GROUPS, RESULT_CASE_FILTER)
+        full_beam_force_query = get_full_beam_force_query(BF_PERM_PARQ_FILE_DICT, BF_EXT_ALS_PARQ_FILE_DICT,
+                                                          BP_EXT_ALS_PARQ_FILE_DICT, beam_numbers, TARGET_GROUPS,
+                                                          RESULT_CASE_FILTER)
 
 
         write_full_beam_forces_query = f"""CREATE TABLE FULL_BEAM_FORCES AS
-                    FULL_BEAM_PROPERTIES AS ({property_query})
+                    WITH FULL_BEAM_PROPERTIES AS ({property_query})
                     {full_beam_force_query};
 
                     COPY FULL_BEAM_FORCES
@@ -186,21 +204,18 @@ if __name__ == '__main__':
         # WRITE THE BEAM FORCES TABLE TO A STANDALONE PARQUET FILE
         # ----------------------------------------------------------------------
         log.info("Writing global axis beam forces to parquet...")
+        start = time.time()
         conn.execute(write_full_beam_forces_query)
         conn.commit()
+        end = time.time()
+        duration = end - start
+        log.info(f"Duration: {duration:.2f}s")
 
         # ----------------------------------------------------------------------
         # WRITE THE BEAM PROPERTIES TABLE TO A STANDALONE PARQUET FILE
         # ----------------------------------------------------------------------
         log.info("Writing beam properties to parquet...")
         conn.execute(write_full_beam_properties_query)
-        conn.commit()
-
-        # ----------------------------------------------------------------------
-        # WRITE THE BEAM ENDS TABLE TO A STANDALONE PARQUET FILE
-        # ----------------------------------------------------------------------
-        log.info("Writing beam end information to parquet...")
-        conn.execute(write_beam_ends_query)
         conn.commit()
 
         # ----------------------------------------------------------------------
